af1c170be203f3905144535a5315d8e10fb3380a
CLovesJava <wntjdcl1@gmail.com>
2024-11-08 02:02:50 +0900

    [Refactor] Enum 타입에 description 추가

--- src/main/java/com/sparta/project/domain/enums/OrderStatus.java
+++ src/main/java/com/sparta/project/domain/enums/OrderStatus.java
@@ -1,5 +1,14 @@
 package com.sparta.project.domain.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum OrderStatus {
-    WAITING, APPROVED, CANCELED
+    WAITING("대기"),
+    APPROVED("승인"),
+    CANCELED("취소");
+
+    private final String description;
 }
--- src/main/java/com/sparta/project/domain/enums/PaymentType.java
+++ src/main/java/com/sparta/project/domain/enums/PaymentType.java
@@ -1,5 +1,13 @@
 package com.sparta.project.domain.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum PaymentType {
-    CARD, CASH
+    CARD("카드"),
+    CASH("현금");
+
+    private final String description;
 }
