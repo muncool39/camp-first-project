75f824051b3a0c5cec32d453c28ebb6891cbe145
siwon <muncool39@gmail.com>
2024-11-07 15:39:01 +0900

    [Feat]  접근 권한이 없는 경우의 에러 코드 추가

--- src/main/java/com/sparta/project/exception/ErrorCode.java
+++ src/main/java/com/sparta/project/exception/ErrorCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-
+    // 예시
+    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "접근 권한이 존재하지 않습니다."),
+    ;
     private final HttpStatus httpStatus;
     private final String message;
 
