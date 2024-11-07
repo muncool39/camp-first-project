f6a68d00f9e08d59a681485e5994341bba388b97
CLovesJava <wntjdcl1@gmail.com>
2024-11-08 01:44:50 +0900

    [Fix] BaseEntity 컬럼 수정

--- src/main/java/com/sparta/project/domain/BaseEntity.java
+++ src/main/java/com/sparta/project/domain/BaseEntity.java
@@ -5,7 +5,9 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -14,7 +16,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity {
     @Column(name="is_deleted") // 삭제 여부
     @ColumnDefault("false")
     private Boolean isDeleted;
@@ -30,13 +32,25 @@ public class BaseEntity {
     @Column(name="deleted_at")
     private LocalDateTime deletedAt;
 
+//    @CreatedBy
     @Column(name="created_by", length=10, updatable = false)
     private String createdBy;
 
-    @LastModifiedDate
+//    @LastModifiedBy
     @Column(name="updated_by", length=10)
-    private LocalDateTime updatedBy;
+    private String updatedBy;
 
     @Column(name="deleted_by", length=10)
-    private LocalDateTime deletedBy;
+    private String deletedBy;
+
+    /*
+    createdBy와 updatedBy는 스프링 시큐리티 부분이 구현되면 AuditorAware 구현체를 사용하는 방법으로 구현하겠습니다.
+    참고: https://javacpro.tistory.com/85
+     */
+
+    public void delete(String username) {
+        this.isDeleted = true;
+        this.deletedAt = LocalDateTime.now();
+        this.deletedBy = username;
+    }
 }
