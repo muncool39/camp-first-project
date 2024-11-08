ffad2ec719223bc5fc86f9b4dbb3bb602603c3ae
siwon <muncool39@gmail.com>
2024-11-09 03:46:46 +0900

    [Feat] 오류 응답 객체에 메세지를 Json 형태로 변환하는 메서드 추가

--- src/main/java/com/sparta/project/exception/ExceptionResponse.java
+++ src/main/java/com/sparta/project/exception/ExceptionResponse.java
@@ -3,4 +3,9 @@ package com.sparta.project.exception;
 public record ExceptionResponse(
         String message
 ) {
+    public String toWrite() {
+        return "{" +
+                "\"message\":" + "\"" + message +"\"" +
+                "}";
+    }
 }
