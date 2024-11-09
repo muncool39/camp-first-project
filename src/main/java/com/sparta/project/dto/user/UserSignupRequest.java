867a10cd549dc2f660a6525ea404645c36aed111
siwon <muncool39@gmail.com>
2024-11-09 14:43:02 +0900

    [Fix] 회원가입 요청 객페의 role 필드 유효성 검사 어노테이션 수정

--- src/main/java/com/sparta/project/dto/user/UserSignupRequest.java
+++ src/main/java/com/sparta/project/dto/user/UserSignupRequest.java
@@ -2,11 +2,12 @@ package com.sparta.project.dto.user;
 
 import com.sparta.project.domain.enums.Role;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record UserSignupRequest(
         @NotBlank String username,
         @NotBlank String password,
         @NotBlank String nickname,
-        @NotBlank Role role
+        @NotNull Role role
 ) {
 }
